380474165ea8b98eb8a72de060a05bdf5c2cd35e
wondieki <wkerubo28@gmail.com>
2024-11-04 10:08:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-function addingEventListener() {
-}
+
+
+// function addingEventListener() {
+// }
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+// const input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  });
